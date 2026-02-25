961732314a72f684e1ce1654d637d5693e72cfe3
CarlosCondor99 <ccondorp@pentalab.tech>
2026-02-25 17:03:35 -0500

    [DEL] modules

--- import_module/models/product_price_list.py
+++
@@ -1,7 +0,0 @@
-# models/product_pricelist.py (crear si no existe ya un archivo que herede)
-from odoo import models, fields
-
-class ProductPricelist(models.Model):
-    _inherit = 'product.pricelist'
-
-    sri_list_price = fields.Boolean(string="Lista SRI", help="Marcar esta lista si se usa para el cálculo del valor original en importaciones.")
--- import_module/wizard/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import lc_taxes_io_wizard
-from . import import_report_wizard # Report Import
